3f42ee4d0dd6cd2e9aa930658eb2efd585e8f777
adasneves127 <adasneves127@gmail.com>
2023-07-12 12:58:00 -0400

    Redid Macros

--- src/6502.cpp
+++ src/6502.cpp
@@ -1,21 +1,79 @@
 #include "6502.h"
 
 
-W65C02::W65C02(){
-    
+
+W65C02::W65C02(void (*update)()){
+    this->update = update;
     
     doReset();
+
+
+    printf("%i", status_reg);
 }
 
 void W65C02::doReset(){
-    PCL = read_mem(0xFFFC);
-    PCH = read_mem(0xFFFD);
+    PCL = readMem(0xFFFC);
+    PCH = readMem(0xFFFD);
     status_reg = 0b00110100;
 }
 
 
 
 void W65C02::clock(){
+    if(BE & ~RDY){
+        fetch();
+        decode();
+        execute();
+        store();
+    }
+}
+
+
+void W65C02::fetch(){
+    this->IR = nextMem();
     
 }
 
+void W65C02::decode(){}
+
+void W65C02::execute(){}
+
+void W65C02::store(){}
+
+
+__uint8_t W65C02::readMem(__uint16_t addr){
+    this->output_pins |= 0b000100;      // Set the 'RWB' bit
+
+    addressBus = (PCH << 8) | PCL;      // Set the address bus
+
+    this->update();                     // Request a system update
+
+    __uint8_t data = dataBus;           // Get the new data from the bus
+
+    this->output_pins &= 0b111011;      // Clear the RWB bit
+
+    return data;                        // Return the data
+}
+
+
+W65C02::~W65C02(){
+
+}
+
+void W65C02::invalid_opcode(){
+    printf("Invalid Opcode %2x", this->IR);
+}
+
+
+void W65C02::inc_pc(){
+    PCL++;              // Increment the lower half of the PC
+    if(PCL == 0){       // If we roll over to 0
+        PCH++;          // Increment the upper portion.
+    }
+}
+
+__uint8_t W65C02::nextMem(){
+    __uint8_t data = readMem((PCH << 8) | PCL);
+    inc_pc();
+    return data;
+}
--- src/6502.h
+++ src/6502.h
@@ -1,8 +1,40 @@
 #include <cstdio>
 
+
+#define NMI (input_pins)      & 1
+#define RDY (input_pins >> 1) & 1
+#define SOB (input_pins >> 2) & 1
+#define RST (input_pins >> 3) & 1
+#define BE  (input_pins >> 4) & 1
+#define IRQ (input_pins >> 5) & 1
+
+
+#define s_Carry     status_reg |= 0b00000001;
+#define s_Zero      status_reg |= 0b00000010;
+#define s_iDis      status_reg |= 0b00000100;
+#define s_Dec       status_reg |= 0b00001000;
+#define s_Over      status_reg |= 0b01000000;
+#define s_Neg       status_reg |= 0b10000000;
+
+
+#define c_Carry     status_reg &= 0b11111110;
+#define c_Zero      status_reg &= 0b11111101;
+#define c_iDis      status_reg &= 0b11111011;
+#define c_Dec       status_reg &= 0b11110111;
+#define c_Over      status_reg &= 0b10111111;
+#define c_Neg       status_reg &= 0b01111111;
+
+#define g_Carry     (status_reg >> 1)   & 1
+#define g_Zero      (status_reg >> 2)   & 1
+#define g_iDis      (status_reg >> 4)   & 1
+#define g_Dec       (status_reg >> 8)   & 1
+#define g_Over      (status_reg >> 64)  & 1
+#define g_Neg       (status_reg >> 128) & 1
+
+
 class W65C02 {
     public:
-        W65C02();
+        W65C02(void (*update)());
         ~W65C02();
 
         void doReset();
@@ -12,6 +44,7 @@ class W65C02 {
 
         void clock();
 
+        __uint8_t output_pins, input_pins;
     private:
         __uint8_t A, X, Y;
         __uint8_t *pA, *pX, *pY;
@@ -23,4 +56,23 @@ class W65C02 {
         __uint8_t SP;
 
         __uint8_t status_reg;
+
+        __uint8_t readMem(__uint16_t addr);
+        void writeMem(__uint16_t addr, __uint8_t data);
+
+
+        void (*update)();
+
+        void push(__uint8_t);
+        __uint8_t pop();
+
+
+        void invalid_opcode();
+
+        void fetch(), decode(), execute(), store();
+
+
+        __uint8_t nextMem();
+
+        void inc_pc();
 };
